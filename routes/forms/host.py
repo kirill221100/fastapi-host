56aa5a9a27683d60742e1f72d502d1114e64c812
kirill221100 <ktagilcev@gmail.com>
2022-09-24 07:01:33 +0700

    rotes and forms

--- routes/forms/host.py
+++ routes/forms/host.py
@@ -19,11 +19,12 @@ class HostForm:
     async def is_valid(self):
         if ' ' in self.url:
             self.errors.append("Route shouldn't contain spaces")
+        elif self.url[0] == '/' or self.url[-1] == '/':
+            self.errors.append("Route shouldn't start or end with '/'")
         elif '\\' in self.url:
             self.errors.append("Route shouldn't contain backslashes")
         elif '' in self.url.split('/'):
             self.errors.append("Too much '/'")
-        elif self.url[0] == '/' or self.url[-1] == '/':
-            self.errors.append("Route shouldn't start or end with '/'")
+
         # elif self.url.split('/')[0] in ['create', 'delete_page', 'update_page', 'login', 'register', 'logout']:
         #     self.errors.append('Make another route')
